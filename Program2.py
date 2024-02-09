0394a518d748193e3ffc996be40c74bb9380d0e2
Sathvik <139783757+Sath2023@users.noreply.github.com>
2024-02-09 04:47:56 +0000

    lol

--- Program2.py
+++ Program2.py
@@ -6,3 +6,12 @@ if(a<0):
     print(num)
 else:
     print("The number is a positive number")
+
+
+print("")
+# Write a python program to check whether the given number is even or odd
+b = int(input("Enter a number :"))
+if((b/2)==0):
+    print("The number is a positve number")
+else:
+    print("The number is an odd number")
