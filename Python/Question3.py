6ad1e6144faf38fbe9a243734df67ff80956d094
Sathvik <139783757+Sath2023@users.noreply.github.com>
2024-02-09 05:22:35 +0000

    final

--- Python/Question3.py
+++ Python/Question3.py
@@ -2,7 +2,7 @@
 a = str(input("Enter an alphabet : "))
 vowel = ['a','e','i','o','u','A','E','I','O','U']
 for i in vowel:
-    if a=i:
+    if a==i:
         print("The given character is a vowel")
         break
 else:
