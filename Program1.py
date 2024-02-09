3c9a09fcf2b9f2f44f23a31c8198536ede72ac29
Sathvik <139783757+Sath2023@users.noreply.github.com>
2024-02-09 04:25:47 +0000

    lololol

--- Program1.py
+++ Program1.py
@@ -4,12 +4,14 @@ print(a/b)
 print(a//b)
 
 #Finding the address of the variables
+print("")
 c = 20
 d = c
 print(id(c))
 print(id(d))
 
 #Type
+print("")
 e = 10
 f = 10.4
 g = "Naanu Naagarahaavu"
@@ -17,12 +19,12 @@ print(type(e))
 print(type(f))
 print(type(g))
 
-
+print("")
 h,i=20,10
 print("This is the values of h",h)
 print("This is the value of i",i)
 
-
+print("")
 j = int(10.5)
 k = float(10)
 print(j,type(j))
