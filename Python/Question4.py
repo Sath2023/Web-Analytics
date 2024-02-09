1ee8ac997c66d08513c7f6ffffbc5713b0ff2542
Sathvik <139783757+Sath2023@users.noreply.github.com>
2024-02-09 05:16:10 +0000

    lol

---
+++ Python/Question4.py
@@ -0,0 +1,7 @@
+"""Write a pyhton program to check the group of box according to the weight condition
+#Cond 1 : 50 - 60 --> Group a
+#Cond 2 : 61 - 70 --> Group B
+Cond 2 : 71 - 80 --> Group C
+Cond 2 : >80 --> Group D"""
+
+"""Check for the logics of Armstrong Number, Palindrome, Spy Number, Neon Number, Fibonacci Number, Prime Number, Happy Number"""
