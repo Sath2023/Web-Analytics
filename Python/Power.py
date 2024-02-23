775f84ee0732c68c096f76e1a494435aa4e668b7
Sathvik <139783757+Sath2023@users.noreply.github.com>
2024-02-23 05:16:07 +0000

    final

---
+++ Python/Power.py
@@ -0,0 +1,7 @@
+num = 9
+i = 15
+power = 1
+while(i>0):
+    power = power * num
+    i = i-1
+print(power)
--- Python/SpyNumbers.py
+++ Python/SpyNumbers.py
@@ -1,12 +1,12 @@
-n = int(input("Enter the number :"))
+num = int(input("Enter a number :"))
 sum = 0
 product = 1
-num = n
-while(num > 0):
-    r = num % 10
-    sum = sum + r
-    product = product * r
-    num = num // 10
+temp = num
+while(temp > 0):
+    rem = temp % 10
+    sum = sum + rem
+    product = product * rem
+    temp = temp // 10
 if(sum == product):
     print("It is spy number")
 else:
