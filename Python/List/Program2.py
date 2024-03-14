023fe4fdf8427159672984d43b616135db95c842
Sathvik <139783757+Sath2023@users.noreply.github.com>
2024-03-14 04:28:31 +0000

    lol

---
+++ Python/List/Program2.py
@@ -0,0 +1,5 @@
+l = [1,2,3,4,5,6,7,8,9,10]
+print("The even numbers in the list are : ")
+for i in l:
+    if(i%2==0):
+        print(i)
---
+++ Python/List/Program4.py
@@ -0,0 +1,5 @@
+l = [12,21,36,55,48,6,7]
+print("THe elements in even index are :")
+for i in range(len(l)):
+    if(i%2==0):
+        print(l[i])
