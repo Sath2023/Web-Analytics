9bac34b0164fca544675accc71568799112ce6a1
Sathvik <139783757+Sath2023@users.noreply.github.com>
2024-03-17 14:24:45 +0000

    final

---
+++ Python/List/Program8.py
@@ -0,0 +1,6 @@
+list = []
+num = int(input("Enter the length of the list : "))
+for i in range(num):
+    element = int(input("Enter the numbers : "))
+    list.append(element)
+print(list)
---
+++ Python/List/Program9.py
@@ -0,0 +1,3 @@
+a = input()
+list = [int(x) for x in a.split(" ")]
+print(list)
